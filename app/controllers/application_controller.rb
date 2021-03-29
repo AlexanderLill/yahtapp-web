e7b8de64730e8a83ecc5b293d9e10adc7eea288d
Michael Ziörjen <mighty.miczed@gmail.com>
2021-03-30 00:44:25 +0200

    Fix registration bug and add redirect to dashboard after signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # configures the permitted parameters for devise (i.e. used on registration / sign up)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:firstname,:lastname, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname,:lastname, :email, :password, :password_confirmation])
+  end
 end
---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+  def after_inactive_sign_up_path_for(resource)
+    dashboard_path
+  end
+end
