062a9a1c85f8aaf56ae207b9df93ff2d440297cc
Michael Ziörjen <mighty.miczed@gmail.com>
2021-04-07 01:15:27 +0200

    Fix typo in migration

--- db/migrate/20210406094518_change_recurrence_column_name.rb
+++ db/migrate/20210406094518_change_recurrence_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeRecurrenceColumnName < ActiveRecord::Migration[6.1]
   def change
-    rename_column :habits, :reccurence, :schedule
+    rename_column :habits, :recurrence, :schedule
   end
 end
