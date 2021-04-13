08f19e10b7d942fb7bf97a78fb0ae3320ff4b02a
Michael Ziörjen <mighty.miczed@gmail.com>
2021-04-13 11:47:04 +0200

    Disabled purging of base css classes in tailwind

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,13 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
-    './app/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/javascript/**/*.vue',
-    './app/javascript/**/*.jsx',
-  ],
+  purge: {
+    layers: ['components', 'utilities'],
+    content:[
+      './app/**/*.html.erb',
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+      './app/javascript/**/*.vue',
+      './app/javascript/**/*.jsx',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
