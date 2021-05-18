4b9e48afe598811a7a74146afdb8632a376c30a8
Sebastian Richner <srichner@outlook.com>
2021-05-18 11:01:32 +0200

    Add more data to self-experimentation view

--- app/controllers/experimentation_controller.rb
+++ app/controllers/experimentation_controller.rb
@@ -1,20 +1,59 @@
 class ExperimentationController < ApplicationController
   before_action :authenticate_user!
   layout 'boxed' # sets the layout for all views with this controller
-  def index
-    reflections = current_user.habit_reflections.includes(habit: :goal)
-                               .where('habit_reflections.created_at <= ?', DateTime.now).order('habit_reflections.created_at')
 
-    @goals = reflections.map(&:habit).uniq.group_by(&:goal_id)
-    habit_reflections = reflections.group_by { |habit_ref| habit_ref.habit.id }
-    @habit_avg_ratings = {}
-    habit_reflections.each do |key, habit_refs|
+  def index
+    param_start = params[:start].to_s
+    @start_datetime = DateTime.parse(param_start)
+
+    pre_reflections = current_user.habit_reflections.includes(habit: :goal)
+                                  .where('habit_reflections.created_at < ?', @start_datetime).order('habit_reflections.created_at')
+
+    post_reflections = current_user.habit_reflections.includes(habit: :goal)
+                                  .where('habit_reflections.created_at >= ?', @start_datetime).order('habit_reflections.created_at')
+
+    @goals = post_reflections.map(&:habit).uniq.group_by(&:goal_id)
+
+    pre_habit_reflections = pre_reflections.group_by { |habit_ref| habit_ref.habit.id }
+    post_habit_reflections = post_reflections.group_by { |habit_ref| habit_ref.habit.id }
+
+
+    @pre_habit_avg_ratings = {}
+    pre_habit_reflections.each do |key, habit_refs|
       sum = 0.0
       habit_refs.each do |habit_ref|
         sum += habit_ref.rating
       end
-      @habit_avg_ratings[key] = sum / habit_refs.size
+      @pre_habit_avg_ratings[key] = sum / habit_refs.size
     end
+
+    @post_habit_avg_ratings = {}
+    post_habit_reflections.each do |key, habit_refs|
+      sum = 0.0
+      habit_refs.each do |habit_ref|
+        sum += habit_ref.rating
+      end
+      @post_habit_avg_ratings[key] = sum / habit_refs.size
+    end
+
+    @pre_habit_reflections_count = {}
+    pre_habit_reflections.each do |key, habit_refs|
+      sum = 0
+      habit_refs.each do |habit_ref|
+        sum = sum + 1
+      end
+      @pre_habit_reflections_count[key] = sum
+    end
+
+    @post_habit_reflections_count = {}
+    post_habit_reflections.each do |key, habit_refs|
+      sum = 0
+      habit_refs.each do |habit_ref|
+        sum = sum + 1
+      end
+      @post_habit_reflections_count[key] = sum
+    end
+
 
   end
 end
