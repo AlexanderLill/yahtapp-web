4c038bbc138a08ccd5c45d235a6b502c86b2f81a
Michael Ziörjen <mighty.miczed@gmail.com>
2021-07-09 17:05:18 +0200

    Add colors to goals and habits in lists, clean up habit detail page

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,8 +1,12 @@
 class Goal < ApplicationRecord
-  belongs_to :user
+
   has_many :derivatives, class_name: 'Goal', foreign_key: 'template_id'
   has_many :experience_sample_configs
+  has_many :habits
+
   belongs_to :template, class_name: 'Goal', optional: true
+  belongs_to :user
+
   validates :title, presence: true
   validates :is_template, inclusion: [false], if: :template_id? # cannot be template if associated with template
   validates :template_id, absence: true, if: :is_template? # must not have association to template if is_template
